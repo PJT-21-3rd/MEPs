7aad41fc0c2fa62025f28b6b773dc952e6296cc6
여인범 <inbeom1202@gmail.com>
2026-07-24 17:48:37 +0900

    chore(fe): 프로젝트 폴더 구조 및 css, 파일설정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div class="min-h-screen bg-gray-50 text-gray-900">
+    <main class="h-[calc(100vh-64px)]">
+      <RouterView />
+    </main>
+  </div>
 </template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
---
+++ frontend/src/api/axios.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// Request Interceptor: JWT 토큰 자동 첨부
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error),
+)
+
+export default api
