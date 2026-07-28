312eba7e6758ab0b935fb70d9085c7ecebfe19fc
여인범 <inbeom1202@gmail.com>
2026-07-28 14:43:03 +0900

    feat(fe): 통합 검색창 컴포넌트 구현

---
+++ frontend/src/components/map/SearchBar.story.vue
@@ -0,0 +1,11 @@
+<template>
+  <Story title="SearchBar" group="map">
+    <Variant title="기본 (Default)">
+      <div class="p-1"><SearchBar /></div>
+    </Variant>
+  </Story>
+</template>
+
+<script setup>
+import SearchBar from './SearchBar.vue'
+</script>
