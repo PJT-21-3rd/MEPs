673d697f63cde2740a9a58ae498af2331577d08e
여인범 <inbeom1202@gmail.com>
2026-08-05 13:40:41 +0900

    style: css 수정

--- frontend/src/components/detail/BuildingInfoPannel.vue
+++ frontend/src/components/detail/BuildingInfoPannel.vue
@@ -12,26 +12,23 @@ const hasBuildingName = computed(() => {
 
 <template>
   <div v-if="buildingData">
-    <h2
-      v-if="hasBuildingName"
-      class="mt-1 text-[20px] leading-tight tracking-tight text-neutral-900"
-    >
+    <h2 v-if="hasBuildingName" class="mt-1 text-[20px] leading-tight tracking-tight text-text-main">
       {{ buildingData.bldNm }}
     </h2>
     <div class="mt-1.5 space-y-1">
       <div class="flex items-center gap-1.5">
-        <span class="rounded bg-neutral-100 px-1.5 py-0.5 text-[12px] text-neutral-500">
+        <span class="rounded bg-surface-gray px-1.5 py-0.5 text-[12px] text-text-secondary">
           지번
         </span>
-        <p class="text-neutral-700" :class="hasBuildingName ? 'text-[15px]' : 'text-[20px] '">
+        <p class="text-text-secondary" :class="hasBuildingName ? 'text-[15px]' : 'text-[20px] '">
           {{ buildingData.jibunAddr }}
         </p>
       </div>
       <div class="flex items-center gap-1.5">
-        <span class="rounded bg-neutral-100 px-1.5 py-0.5 text-[12px] text-neutral-400">
+        <span class="rounded bg-surface-gray px-1.5 py-0.5 text-[12px] text-text-sub">
           도로명
         </span>
-        <p class="text-neutral-400" :class="hasBuildingName ? 'text-[14px]' : 'text-[15px]'">
+        <p class="text-text-sub" :class="hasBuildingName ? 'text-[14px]' : 'text-[15px]'">
           {{ buildingData.roadAddr }}
         </p>
       </div>
