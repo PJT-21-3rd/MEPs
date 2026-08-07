6f0e8310a521e5b3dc851ef90513d7fa70d9ee37
hongseoyoung <hsyeong0201@gmail.com>
2026-08-07 10:09:38 +0900

    fix(fe): 대출 모달 기능 구현달

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="w-screen h-screen overflow-hidden">
     <RouterView />
+    <InsuranceModal
+      v-if="uiStore.insuranceModalConfig"
+      v-bind="uiStore.insuranceModalConfig"
+      @close="uiStore.closeInsuranceModal"
+      @submit="uiStore.closeInsuranceModal"
+    />
+    <LoanModal
+      v-if="uiStore.loanModalConfig"
+      v-bind="uiStore.loanModalConfig"
+      @close="uiStore.closeLoanModal"
+      @view-detail="(product) => console.log('상세보기', product)"
+      @find-branch="() => {}"
+      @open-app="() => {}"
+    />
   </div>
 </template>
 
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
+import { useUiStore } from '@/stores/uiStore';
+import InsuranceModal from '@/components/finance/InsuranceModal.vue';
+import LoanModal from '@/components/finance/LoanModal.vue';
+
+const uiStore = useUiStore();
 </script>
