8ac97f4ea22e32527f06b2e827cde73155dfa141
gkscodus1001-design <gkscodus1001@gmail.com>
2026-08-10 15:08:55 +0900

    feat(fe) : 프로필 드롭다운 UI 구현

---
+++ frontend/src/components/auth/ProfileDropdown.story.vue
@@ -0,0 +1,13 @@
+<script setup>
+import ProfileDropdown from './ProfileDropdown.vue';
+</script>
+
+<template>
+  <Story title="인증/ProfileDropdown">
+    <Variant title="기본">
+      <div class="p-8 bg-surface-gray flex justify-end">
+        <ProfileDropdown @logout="() => {}" @delete-account="() => {}" />
+      </div>
+    </Variant>
+  </Story>
+</template>
