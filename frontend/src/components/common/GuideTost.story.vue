4dbac00f09f189c07bfb837eb35bdeb2b1b0a8ae
gkscodus1001-design <gkscodus1001@gmail.com>
2026-08-11 11:17:41 +0900

    feat(fe) : 지도 위 안내 토스트 컴포넌트 구현

---
+++ frontend/src/components/common/GuideTost.story.vue
@@ -0,0 +1,13 @@
+<script setup>
+import GuideToast from './GuideToast.vue';
+</script>
+
+<template>
+  <Story title="공통/GuideToast">
+    <Variant title="기본">
+      <div class="p-8 bg-surface-gray">
+        <GuideToast @close="() => {}" />
+      </div>
+    </Variant>
+  </Story>
+</template>
