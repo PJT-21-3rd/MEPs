78a7d1528ed292ed4b38b792c0b61c49f9ad03f9
hongseoyoung <hsyeong0201@gmail.com>
2026-08-21 14:19:22 +0900

    리포트 하단 문구 수정

--- frontend/src/components/report/DetailedReportDisclaimer.vue
+++ frontend/src/components/report/DetailedReportDisclaimer.vue
@@ -13,11 +13,14 @@ const today = computed(() => {
 </script>
 
 <template>
-  <p class="text-[12px] text-text-disabled text-center leading-relaxed px-2">
-    본 리포트는 국토교통부 건축물대장, 소방청 화재이력 DB, 기상청 침수 이력 데이터를 기반으로 AI가
-    분석했습니다.
+  <br />
+  <p class="text-[12px] text-text-disabled text-center leading-relaxed px-8">
+    본 리포트는 국토교통부, 행정안전부, 서울시 공공데이터
   </p>
-  <p class="text-[12px] text-text-disabled text-center leading-relaxed px-2">
+  <p class="text-[12px] text-text-disabled text-center leading-relaxed px-8">
+    기반으로 AI가 분석했습니다.
+  </p>
+  <p class="text-[12px] text-text-disabled text-center leading-relaxed px-8">
     분석 기준일: {{ today }}
   </p>
 </template>
